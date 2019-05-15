b61dab6724ee73d9e60c5e8bf082b4f10485ecb6
Ike Njoku <ikeenjoku@gmail.com>
2019-05-15 11:10:44 +0100

    auth protect location endpoints

--- src/modules/location/index.js
+++ src/modules/location/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import tokenizer from '../../helpers/tokenizer'
 import LocationController from './LocationController';
 import validateLocation from '../../middlewares/validateLocation';
 
@@ -13,31 +14,37 @@ Router.post(
 
 Router.post(
   '/locations/:locationId/sublocations',
+  tokenizer.verifyToken,
   LocationController.createSublocation,
 );
 
 Router.get(
   '/locations',
+  tokenizer.verifyToken,
   LocationController.getAllLocations,
 );
 
 Router.get(
   '/locations/summary',
+  tokenizer.verifyToken,
   LocationController.summarizeLocationData,
 );
 
 Router.get(
   '/locations/:locationId',
+  tokenizer.verifyToken,
   LocationController.getLocation,
 );
 
 Router.put(
   '/locations/:locationId',
+  tokenizer.verifyToken,
   LocationController.updateLocation,
 );
 
 Router.delete(
   '/locations/:locationId',
+  tokenizer.verifyToken,
   LocationController.deleteLocation,
 );
 
