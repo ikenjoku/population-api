1d1db48ffb36550f8d94057fc9bb2fdf1e0265c8
Ike Njoku <ikeenjoku@gmail.com>
2019-05-15 08:31:06 +0100

    add handler to create sublocations

--- src/modules/location/LocationController.js
+++ src/modules/location/LocationController.js
@@ -2,13 +2,13 @@ import Location from '../../data/models/location';
 
 class LocationController {
   static createLocation(req, res, next) {
-    const { male, female } = req.body;
-    if (!male || !female) {
-      const err = new Error('Please specify the number of male and female citizens');
+    const { male, female, area } = req.body;
+    if (!male || !female || !area) {
+      const err = new Error('Please specify the number of male and female citizens in the area');
       err.status = 400;
       next(err);
     } else {
-      const newLocation= new Location({ male, female });
+      const newLocation= new Location({ area, male, female });
       newLocation.save((err, location) => {
         if (err) return next(err);
         return res.status(201).json({
@@ -20,9 +20,14 @@ class LocationController {
   }
 
   static createSublocation(req, res, next) {
-    return res.status(201).json({
-      message: 'createSublocation',
-    });
+    const { male, female } = req.body;
+    req.location.male += male;
+    req.location.female += female;
+    req.location.locations.push(req.body);
+  req.location.save(function(err, location){
+    if (err) return next(err);
+    res.status(201).json(location);
+  })
   }
 
   static getLocations(req, res, next) {
